b38207a0fb1851f0675f98049bdc55f1275236f7
jammy0101 <walker0101walker.gmail.com>
2025-06-14 12:29:08 +0500

    Image uploaded

--- lib/UploadImageScreen.dart
+++ lib/UploadImageScreen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class UploadImageScreen extends StatefulWidget {
   const UploadImageScreen({super.key});
@@ -8,20 +12,104 @@ class UploadImageScreen extends StatefulWidget {
 }
 
 class _UploadImageScreenState extends State<UploadImageScreen> {
+  File? image;
+  final _picker = ImagePicker();
+  bool showSpinner = false;
+
+  Future getImage() async {
+    final pickedFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 80,
+    );
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+      setState(() {});
+    } else {
+      print('NO image is selected');
+    }
+  }
+  Future<void> uploadImage() async {
+    setState(() {
+      showSpinner = true;
+    });
+    var stream = new http.ByteStream(image!.openRead());
+    stream.cast();
+    var length = await image!.length();
+    var uri = Uri.parse('https://fakestoreapi.com/products');
+    var request = new http.MultipartRequest('POST', uri);
+    request.fields['title'] = "Static title";
+    var multiport = new http.MultipartFile(
+        'image',
+        stream,
+        length
+    );
+    request.files.add(multiport);
+    var response = await request.send();
+    if(response.statusCode == 200){
+      setState(() {
+        showSpinner = false;
+      });
+
+      print('Image Uploaded');
+    }else{
+      setState(() {
+        showSpinner = false;
+      });
+
+      print('Failed');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Upload file/image to server Api'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          
-        ],
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Upload file/image to server Api'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: (){
+                getImage();
+              },
+              child: Container(
+                child: image == null
+                    ? Center(child: Text('Pick Image'))
+                    : Container(
+                        child: Center(
+                          child: Image.file(
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                            File(image!.path).absolute,
+                          ),
+                        ),
+                      ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: (){
+                uploadImage();
+              },
+              child: Container(
+                height: 40,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(child: Text('Upload')),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
